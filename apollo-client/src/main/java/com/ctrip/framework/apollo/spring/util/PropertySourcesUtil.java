f6670d5584c2423af570916b42f1077030ff6bbd
Gallant <gallantkong@163.com>
2022-11-15 09:50:00 +0800

    repair the application registerBeanDefinitions read not appropriate c… (#3)
    * repair the application registerBeanDefinitions read not appropriate configuration,cause apollo config wasn't the first configuration in environment during registerBeanDefinitions to postProcessBeanFactory,when spring.cloud.bootstrap.enabled is set to true
    
    * refactor CHANGES.md
    
    * refactor CHANGES.md
    
    * Apply suggestions from code review
    
    Co-authored-by: Jason Song <nobodyiam@gmail.com>

---
+++ apollo-client/src/main/java/com/ctrip/framework/apollo/spring/util/PropertySourcesUtil.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2022 Apollo Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.ctrip.framework.apollo.spring.util;
+
+import com.ctrip.framework.apollo.spring.config.PropertySourcesConstants;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertySource;
+
+/**
+ * @author Gallant
+ */
+public class PropertySourcesUtil {
+
+
+  /**
+   * Ensure ApolloBootstrapPropertySources is still the first
+   * @param environment : Ensure ApolloBootstrapPropertySources is still the first in the environment
+   */
+  public static void ensureBootstrapPropertyPrecedence(ConfigurableEnvironment environment) {
+    MutablePropertySources propertySources = environment.getPropertySources();
+
+    PropertySource<?> bootstrapPropertySource = propertySources
+        .get(PropertySourcesConstants.APOLLO_BOOTSTRAP_PROPERTY_SOURCE_NAME);
+
+    // not exists or already in the first place
+    if (bootstrapPropertySource == null || propertySources.precedenceOf(bootstrapPropertySource) == 0) {
+      return;
+    }
+
+    propertySources.remove(PropertySourcesConstants.APOLLO_BOOTSTRAP_PROPERTY_SOURCE_NAME);
+    propertySources.addFirst(bootstrapPropertySource);
+  }
+
+}
