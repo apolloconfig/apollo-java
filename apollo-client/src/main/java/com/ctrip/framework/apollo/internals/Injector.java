62412e18467012965080367d7a072acf36dce8d4
Jason Song <songshun.ss@antfin.com>
2023-01-18 22:14:08 +0800

    add Ordered interface to MessageProducerManager and Injector SPI

--- apollo-client/src/main/java/com/ctrip/framework/apollo/internals/Injector.java
+++ apollo-client/src/main/java/com/ctrip/framework/apollo/internals/Injector.java
@@ -16,10 +16,12 @@
  */
 package com.ctrip.framework.apollo.internals;
 
+import com.ctrip.framework.apollo.core.spi.Ordered;
+
 /**
  * @author Jason Song(song_s@ctrip.com)
  */
-public interface Injector {
+public interface Injector extends Ordered {
 
   /**
    * Returns the appropriate instance for the given injection type
@@ -30,4 +32,9 @@ public interface Injector {
    * Returns the appropriate instance for the given injection type and name
    */
   <T> T getInstance(Class<T> clazz, String name);
+
+  @Override
+  default int getOrder() {
+    return 0;
+  }
 }
--- apollo-core/src/main/java/com/ctrip/framework/apollo/tracer/spi/MessageProducerManager.java
+++ apollo-core/src/main/java/com/ctrip/framework/apollo/tracer/spi/MessageProducerManager.java
@@ -16,12 +16,19 @@
  */
 package com.ctrip.framework.apollo.tracer.spi;
 
+import com.ctrip.framework.apollo.core.spi.Ordered;
+
 /**
  * @author Jason Song(song_s@ctrip.com)
  */
-public interface MessageProducerManager {
+public interface MessageProducerManager extends Ordered {
   /**
    * @return the message producer
    */
   MessageProducer getProducer();
+
+  @Override
+  default int getOrder() {
+    return 0;
+  }
 }
