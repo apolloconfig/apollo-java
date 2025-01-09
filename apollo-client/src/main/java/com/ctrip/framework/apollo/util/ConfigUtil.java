24kpure/apollo

feat: reduce conflicts when update configmap in k8s #89

apollo-client/src/main/java/com/ctrip/framework/apollo/util/ConfigUtil.java
@@ -72,11 +72,13 @@ public class ConfigUtil {
   private boolean propertyFileCacheEnabled = true;
   private boolean overrideSystemProperties = true;
   private boolean propertyKubernetesCacheEnabled = false;
+  private int propertyKubernetesMaxWritePods = 3;
   private boolean clientMonitorEnabled = false;
   private boolean clientMonitorJmxEnabled = false;
   private String monitorExternalType = "";
   private long monitorExternalExportPeriod = 10;
   private int monitorExceptionQueueSize = 25;
+
 
   public ConfigUtil() {
     warnLogRateLimiter = RateLimiter.create(0.017); // 1 warning log output per minute
@@ -93,6 +95,7 @@ public class ConfigUtil {
     initPropertyFileCacheEnabled();
     initOverrideSystemProperties();
     initPropertyKubernetesCacheEnabled();
+    initPropertyKubernetesMaxWritePods();
     initClientMonitorEnabled();
     initClientMonitorJmxEnabled();
     initClientMonitorExternalType();
@@ -390,31 +393,44 @@ public class ConfigUtil {
   }
 
   public String getK8sNamespace() {
-    String k8sNamespace = getCacheKubernetesNamespace();
-
-    if (!Strings.isNullOrEmpty(k8sNamespace)) {
-      return k8sNamespace;
-    }
-
-    return ConfigConsts.KUBERNETES_CACHE_CONFIG_MAP_NAMESPACE_DEFAULT;
+    return getK8sConfigProperties(ApolloClientSystemConsts.APOLLO_CACHE_KUBERNETES_NAMESPACE,
+            ApolloClientSystemConsts.APOLLO_CACHE_KUBERNETES_NAMESPACE_ENVIRONMENT_VARIABLES,
+            ConfigConsts.KUBERNETES_CACHE_CONFIG_MAP_NAMESPACE_DEFAULT);
   }
 
-  private String getCacheKubernetesNamespace() {
+  private void initPropertyKubernetesMaxWritePods() {
+    String propertyKubernetesMaxWritePodsStr = getK8sConfigProperties(ApolloClientSystemConsts.APOLLO_CACHE_KUBERNETES_MAX_WRITE_PODS,
+            ApolloClientSystemConsts.APOLLO_CACHE_MAX_WRITE_PODS_ENVIRONMENT_VARIABLES,
+            String.valueOf(propertyKubernetesMaxWritePods));
+    if (!Strings.isNullOrEmpty(propertyKubernetesMaxWritePodsStr)) {
+      try {
+        propertyKubernetesMaxWritePods = Integer.parseInt(propertyKubernetesMaxWritePodsStr);
+      } catch (Throwable ex) {
+        logger.error("Config for {} is invalid: {}",
+                ApolloClientSystemConsts.APOLLO_CACHE_KUBERNETES_NAMESPACE, propertyKubernetesMaxWritePodsStr);
+      }
+    }
+  }
+
+  private String getK8sConfigProperties(String key, String environmentKey, String defaultValue) {
     // 1. Get from System Property
-    String k8sNamespace = System.getProperty(ApolloClientSystemConsts.APOLLO_CACHE_KUBERNETES_NAMESPACE);
+    String k8sNamespace = System.getProperty(key);
     if (Strings.isNullOrEmpty(k8sNamespace)) {
       // 2. Get from OS environment variable
-      k8sNamespace = System.getenv(ApolloClientSystemConsts.APOLLO_CACHE_KUBERNETES_NAMESPACE_ENVIRONMENT_VARIABLES);
+      k8sNamespace = System.getenv(environmentKey);
     }
     if (Strings.isNullOrEmpty(k8sNamespace)) {
       // 3. Get from server.properties
-      k8sNamespace = Foundation.server().getProperty(ApolloClientSystemConsts.APOLLO_CACHE_KUBERNETES_NAMESPACE, null);
+      k8sNamespace = Foundation.server().getProperty(key, null);
     }
     if (Strings.isNullOrEmpty(k8sNamespace)) {
       // 4. Get from app.properties
-      k8sNamespace = Foundation.app().getProperty(ApolloClientSystemConsts.APOLLO_CACHE_KUBERNETES_NAMESPACE, null);
+      k8sNamespace = Foundation.app().getProperty(key, null);
     }
-    return k8sNamespace;
+    if (!Strings.isNullOrEmpty(k8sNamespace)) {
+      return k8sNamespace;
+    }
+    return defaultValue;
   }
 
   public boolean isInLocalMode() {
@@ -522,6 +538,10 @@ public class ConfigUtil {
 
   public boolean isPropertyKubernetesCacheEnabled() {
     return propertyKubernetesCacheEnabled;
+  }
+
+  public int getPropertyKubernetesMaxWritePods() {
+    return propertyKubernetesMaxWritePods;
   }
 
   public boolean isOverrideSystemProperties() {

apollo-core/src/main/java/com/ctrip/framework/apollo/core/ApolloClientSystemConsts.java
@@ -84,6 +84,16 @@ public class ApolloClientSystemConsts {
   public static final String APOLLO_CACHE_KUBERNETES_NAMESPACE_ENVIRONMENT_VARIABLES = "APOLLO_CACHE_KUBERNETES_NAMESPACE";
 
   /**
+   * max number of pods that can write the configmap cache in Kubernetes
+   */
+  public static final String APOLLO_CACHE_KUBERNETES_MAX_WRITE_PODS = "apollo.cache.kubernetes.max-write-pods";
+
+  /**
+   * max number of pods that can write the configmap cache in Kubernetes environment variables
+   */
+  public static final String APOLLO_CACHE_MAX_WRITE_PODS_ENVIRONMENT_VARIABLES = "APOLLO_CACHE_MAX_WRITE_PODS_NAMESPACE";
+
+  /**
    * apollo client access key
    */
   public static final String APOLLO_ACCESS_KEY_SECRET = "apollo.access-key.secret";

apolloconfig/apollo-java

apollo-client/src/main/java/com/ctrip/framework/apollo/util/ConfigUtil.java
@@ -72,11 +72,13 @@ public class ConfigUtil {
   private boolean propertyFileCacheEnabled = true;
   private boolean overrideSystemProperties = true;
   private boolean propertyKubernetesCacheEnabled = false;
+  private int propertyKubernetesMaxWritePods = 3;
   private boolean clientMonitorEnabled = false;
   private boolean clientMonitorJmxEnabled = false;
   private String monitorExternalType = "";
   private long monitorExternalExportPeriod = 10;
   private int monitorExceptionQueueSize = 25;
+
 
   public ConfigUtil() {
     warnLogRateLimiter = RateLimiter.create(0.017); // 1 warning log output per minute
@@ -93,6 +95,7 @@ public class ConfigUtil {
     initPropertyFileCacheEnabled();
     initOverrideSystemProperties();
     initPropertyKubernetesCacheEnabled();
+    initPropertyKubernetesMaxWritePods();
     initClientMonitorEnabled();
     initClientMonitorJmxEnabled();
     initClientMonitorExternalType();
@@ -390,31 +393,44 @@ public class ConfigUtil {
   }
 
   public String getK8sNamespace() {
-    String k8sNamespace = getCacheKubernetesNamespace();
-
-    if (!Strings.isNullOrEmpty(k8sNamespace)) {
-      return k8sNamespace;
-    }
-
-    return ConfigConsts.KUBERNETES_CACHE_CONFIG_MAP_NAMESPACE_DEFAULT;
+    return getK8sConfigProperties(ApolloClientSystemConsts.APOLLO_CACHE_KUBERNETES_NAMESPACE,
+            ApolloClientSystemConsts.APOLLO_CACHE_KUBERNETES_NAMESPACE_ENVIRONMENT_VARIABLES,
+            ConfigConsts.KUBERNETES_CACHE_CONFIG_MAP_NAMESPACE_DEFAULT);
   }
 
-  private String getCacheKubernetesNamespace() {
+  private void initPropertyKubernetesMaxWritePods() {
+    String propertyKubernetesMaxWritePodsStr = getK8sConfigProperties(ApolloClientSystemConsts.APOLLO_CACHE_KUBERNETES_MAX_WRITE_PODS,
+            ApolloClientSystemConsts.APOLLO_CACHE_KUBERNETES_MAX_WRITE_PODS_ENVIRONMENT_VARIABLES,
+            String.valueOf(propertyKubernetesMaxWritePods));
+    if (!Strings.isNullOrEmpty(propertyKubernetesMaxWritePodsStr)) {
+      try {
+        propertyKubernetesMaxWritePods = Integer.parseInt(propertyKubernetesMaxWritePodsStr);
+      } catch (Throwable ex) {
+        logger.error("Config for {} is invalid: {}",
+                ApolloClientSystemConsts.APOLLO_CACHE_KUBERNETES_NAMESPACE, propertyKubernetesMaxWritePodsStr);
+      }
+    }
+  }
+
+  private String getK8sConfigProperties(String key, String environmentKey, String defaultValue) {
     // 1. Get from System Property
-    String k8sNamespace = System.getProperty(ApolloClientSystemConsts.APOLLO_CACHE_KUBERNETES_NAMESPACE);
+    String k8sNamespace = System.getProperty(key);
     if (Strings.isNullOrEmpty(k8sNamespace)) {
       // 2. Get from OS environment variable
-      k8sNamespace = System.getenv(ApolloClientSystemConsts.APOLLO_CACHE_KUBERNETES_NAMESPACE_ENVIRONMENT_VARIABLES);
+      k8sNamespace = System.getenv(environmentKey);
     }
     if (Strings.isNullOrEmpty(k8sNamespace)) {
       // 3. Get from server.properties
-      k8sNamespace = Foundation.server().getProperty(ApolloClientSystemConsts.APOLLO_CACHE_KUBERNETES_NAMESPACE, null);
+      k8sNamespace = Foundation.server().getProperty(key, null);
     }
     if (Strings.isNullOrEmpty(k8sNamespace)) {
       // 4. Get from app.properties
-      k8sNamespace = Foundation.app().getProperty(ApolloClientSystemConsts.APOLLO_CACHE_KUBERNETES_NAMESPACE, null);
+      k8sNamespace = Foundation.app().getProperty(key, null);
     }
-    return k8sNamespace;
+    if (!Strings.isNullOrEmpty(k8sNamespace)) {
+      return k8sNamespace;
+    }
+    return defaultValue;
   }
 
   public boolean isInLocalMode() {
@@ -522,6 +538,10 @@ public class ConfigUtil {
 
   public boolean isPropertyKubernetesCacheEnabled() {
     return propertyKubernetesCacheEnabled;
+  }
+
+  public int getPropertyKubernetesMaxWritePods() {
+    return propertyKubernetesMaxWritePods;
   }
 
   public boolean isOverrideSystemProperties() {

apollo-core/src/main/java/com/ctrip/framework/apollo/core/ApolloClientSystemConsts.java
@@ -84,6 +84,16 @@ public class ApolloClientSystemConsts {
   public static final String APOLLO_CACHE_KUBERNETES_NAMESPACE_ENVIRONMENT_VARIABLES = "APOLLO_CACHE_KUBERNETES_NAMESPACE";
 
   /**
+   * max number of pods that can write the configmap cache in Kubernetes
+   */
+  public static final String APOLLO_CACHE_KUBERNETES_MAX_WRITE_PODS = "apollo.cache.kubernetes.max-write-pods";
+
+  /**
+   * max number of pods that can write the configmap cache in Kubernetes environment variables
+   */
+  public static final String APOLLO_CACHE_KUBERNETES_MAX_WRITE_PODS_ENVIRONMENT_VARIABLES = "APOLLO_CACHE_KUBERNETES_MAX_WRITE_PODS";
+
+  /**
    * apollo client access key
    */
   public static final String APOLLO_ACCESS_KEY_SECRET = "apollo.access-key.secret";